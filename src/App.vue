6b40eb01169f918fdbf5ae230219175cd9efc47e
とーと <hibiyahibiyahibiya77th@gmail.com>
2025-08-25 19:39:34 +0900

    feat: implement subtle content

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
     <p>参考映像</p>
   </div>
   <video src="/video.mp4" controls class="responsive-video" />
+  <div class="github-link">
+    <a
+      href="https://github.com/thoth333/Simplified-Clock"
+      target="_blank"
+      rel="noopener"
+    >
+      View on GitHub
+    </a>
+  </div>
 </template>
 
 <style scoped>
@@ -21,5 +30,20 @@ p {
   height: auto;
   display: block;
   margin: 1em auto;
+}
+.github-link {
+  text-align: center;
+  margin-top: 2em;
+}
+.github-link a {
+  color: #24292f;
+  font-weight: 500;
+  text-decoration: none;
+  border-bottom: 1px dotted #24292f;
+  transition: color 0.2s;
+}
+.github-link a:hover {
+  color: #0366d6;
+  border-bottom: 1px solid #0366d6;
 }
 </style>
