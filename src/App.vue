2e035949a8edd737c26aa45bb1627bb04f8e6805
とーと <hibiyahibiyahibiya77th@gmail.com>
2025-08-25 19:30:01 +0900

    feat: implement required features

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>約分時計 <span class="small">Simplified Clock</span></p>
+    <p>参考映像</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <video src="/video.mp4" controls class="responsive-video" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+p {
+  text-align: center;
+  font-size: 3em;
+  margin: 0.3em 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.small {
+  font-size: 0.7em;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.responsive-video {
+  width: 100%;
+  max-width: 600px;
+  height: auto;
+  display: block;
+  margin: 1em auto;
 }
 </style>
